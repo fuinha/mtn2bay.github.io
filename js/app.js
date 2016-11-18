4b27932ee9e11b54de6c8c58eba7c3e3bb41e6e0
mtn2bay <mikemroy@gmail.com>
2016-11-18 13:56:07 -0500

    working at paused carousels till in view

--- js/app.js
+++ js/app.js
@@ -9,20 +9,24 @@ $(".nav-link").on("click", function (){
 	});
 });
 
-// Easy as Pie plugin
-$(function() {
-    setTimeout(function(){
-        $('.chart').easyPieChart({
-        animate: 5000,
-        easing: 'easeOutBounce',
-        onStep: function (from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
-        },
-        barColor: '#05D6AC', //FORCOLOR OF CHART
-        lineWidth: 5, //WIDTH OF STRIP OF SKILL CHART
-        size: 100, //WIDTH - HEIGHT OF SKILL CHART(SHOULD BE IN SQUARE) -  SHOUL BE EQUAL - Like 200px 
-        });
-    }, 3000);
+// Easy as Pie plugin - load when in view
+$('.skill-pie').viewportChecker({
+    offset: 200,                  // wait for the first 200 pixel of the element 
+                                      //to enter in the viewport
+    callbackFunction: function(elem){
+        setTimeout(function(){
+            $('.chart').easyPieChart({
+	        animate: 5000,
+	        easing: 'easeOutBounce',
+	        onStep: function (from, to, percent) {
+	            $(this.el).find('.percent').text(Math.round(percent));
+	        },
+	        barColor: '#05D6AC', //FORCOLOR OF CHART
+	        lineWidth: 5, //WIDTH OF STRIP OF SKILL CHART
+	        size: 100, //WIDTH - HEIGHT OF SKILL CHART(SHOULD BE IN SQUARE) -  SHOUL BE EQUAL - Like 200px 
+	        });
+        },500);
+    }
 });
 
 // Init Google Maps
@@ -49,3 +53,31 @@ $(function init_map() {
 	google.maps.event.addDomListener(window, 'load', init_map);
 });
 
+// Cycle carousels when in view
+$('#about').viewportChecker({
+    offset: 200,
+    callbackFunction: function(elem){
+        
+    	$('#carousel-profile').carousel({
+		  	interval:5000,
+		  	pause:false,
+		  	cycle:true,
+		  	wrap:true
+		});
+        console.log("play");
+        
+    }
+});
+
+$('#examples').viewportChecker({
+    offset: 200,
+    callbackFunction: function(elem){
+        setTimeout(function(){
+            $('#multi-item-example').carousel({
+			  	interval:5000
+			});
+            $('#multi-item-example').carousel('cycle'); // remove pause
+            console.log("play");
+        },500);
+    }
+});
