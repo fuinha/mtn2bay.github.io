20ee1b6e5095950676f2ff6835384d10f09b414d
mtn2bay <mikemroy@gmail.com>
2016-11-18 11:56:22 -0500

    add velocity plugin and move scripts to app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,51 @@
-$(".nav-link").click(function (){
+// Velocity plugin for nav scroll
+$(".nav-link").on("click", function (){
 	event.preventDefault();
 	var target = $(this).attr("href");
-	
-    $("html, body").animate({
-        scrollTop: $(target).offset().top -100
-    }, 2500);
+
+	$(target).velocity("scroll", { 
+		duration: 3000,
+		offset: -100,
+	});
 });
+
+// Easy as Pie plugin
+$(function() {
+    setTimeout(function(){
+        $('.chart').easyPieChart({
+        animate: 5000,
+        easing: 'easeOutBounce',
+        onStep: function (from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        },
+        barColor: '#05D6AC', //FORCOLOR OF CHART
+        lineWidth: 5, //WIDTH OF STRIP OF SKILL CHART
+        size: 100, //WIDTH - HEIGHT OF SKILL CHART(SHOULD BE IN SQUARE) -  SHOUL BE EQUAL - Like 200px 
+        });
+    }, 3000);
+});
+
+// Init Google Maps
+$(function init_map() {
+
+    var var_location = new google.maps.LatLng(43.873032, -70.144720);
+
+    var var_mapoptions = {
+        center: var_location,
+        mapTypeId: 'hybrid',
+        zoom: 9
+    };
+
+    var var_marker = new google.maps.Marker({
+        position: var_location,
+        map: var_map,
+        title: "Pownal"
+    });
+
+    var var_map = new google.maps.Map(document.getElementById("map-container"),
+        var_mapoptions);
+    	var_marker.setMap(var_map);
+
+	google.maps.event.addDomListener(window, 'load', init_map);
+});
+
