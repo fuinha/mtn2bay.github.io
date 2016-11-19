300c4e9632fd1b66b3336cda0c1c5adbb6bb234a
mtn2bay <mikemroy@gmail.com>
2016-11-19 09:08:49 -0500

    adjust pie chart easing

--- js/app.js
+++ js/app.js
@@ -17,14 +17,14 @@ $('.skill-pie').viewportChecker({
                                       //to enter in the viewport
     callbackFunction: function(elem){
         $('.chart').easyPieChart({
-        animate: 3000,
-        easing: 'easeOutBounce',
-        onStep: function (from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
-        },
-        barColor: '#05D6AC', //FORCOLOR OF CHART
-        lineWidth: 5, //WIDTH OF STRIP OF SKILL CHART
-        size: 100, //WIDTH - HEIGHT OF SKILL CHART(SHOULD BE IN SQUARE) -  SHOUL BE EQUAL - Like 200px 
+	        animate: 6000,
+	        easing: 'easeInOutCubic',
+	        onStep: function (from, to, percent) {
+	            $(this.el).find('.percent').text(Math.round(percent));
+	        },
+	        barColor: '#05D6AC', //FORCOLOR OF CHART
+	        lineWidth: 5, //WIDTH OF STRIP OF SKILL CHART
+	        size: 100, //WIDTH - HEIGHT OF SKILL CHART(SHOULD BE IN SQUARE) -  SHOUL BE EQUAL - Like 200px 
         });
     }
 });
